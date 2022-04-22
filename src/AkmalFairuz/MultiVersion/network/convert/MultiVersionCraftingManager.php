ac7d7e13e3ad6d897dd91aaf8f7fea6e176cc97f
FaizxDev <75949240+Faizx511x@users.noreply.github.com>
2022-04-22 10:03:02 +0300

    Fix :D

--- src/AkmalFairuz/MultiVersion/network/convert/MultiVersionCraftingManager.php
+++ src/AkmalFairuz/MultiVersion/network/convert/MultiVersionCraftingManager.php
@@ -38,6 +38,7 @@ class MultiVersionCraftingManager extends CraftingManager{
 	private $caches = [];
 
     const PROTOCOL = [
+	ProtocolConstants::BEDROCK_1_18_30,
 	ProtocolConstants::BEDROCK_1_18_10,
         ProtocolConstants::BEDROCK_1_18_0,
         ProtocolConstants::BEDROCK_1_17_40,
