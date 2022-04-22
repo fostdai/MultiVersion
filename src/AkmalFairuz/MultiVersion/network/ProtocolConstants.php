580971a7cffe248f22f96e985960e988b70aa1f8
FaizxDev <75949240+Faizx511x@users.noreply.github.com>
2022-04-22 09:53:21 +0300

    Fix :D

--- src/AkmalFairuz/MultiVersion/network/ProtocolConstants.php
+++ src/AkmalFairuz/MultiVersion/network/ProtocolConstants.php
@@ -14,6 +14,8 @@ class ProtocolConstants{
     public const BEDROCK_1_17_30 = 465;
     public const BEDROCK_1_17_40 = 471;
     public const BEDROCK_1_18_0 = 475;
+    public const BEDROCK_1_18_10 = 486;
+    public const BEDROCK_1_18_30 = 503;
 
     public const MINECRAFT_VERSION = [
         self::BEDROCK_1_16_220 => "1.16.220",
@@ -22,6 +24,8 @@ class ProtocolConstants{
         self::BEDROCK_1_17_30 => "1.17.30",
         self::BEDROCK_1_17_40 => "1.17.40",
         self::BEDROCK_1_18_0 => "1.18.0",
+        self::BEDROCK_1_18_10 => "1.18.10",
+        self::BEDROCK_1_18_30 => "1.18.30",
         ProtocolInfo::CURRENT_PROTOCOL => ProtocolInfo::MINECRAFT_VERSION_NETWORK
     ];
 
@@ -32,6 +36,8 @@ class ProtocolConstants{
         self::BEDROCK_1_17_30,
         self::BEDROCK_1_17_40,
         self::BEDROCK_1_18_0,
+        self::BEDROCK_1_18_10,
+        self::BEDROCK_1_18_30,
         ProtocolInfo::CURRENT_PROTOCOL
     ];
 }
